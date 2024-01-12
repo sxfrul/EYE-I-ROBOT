3fdfe9d5abc0b658fe7b1be7c2f4b5ec09df007b
sxfrul <muhdsafrul@gmail.com>
2024-01-12 21:03:16 +0800

    + newer more scalable homeAutomation script

--- homeAutomation.py
+++ homeAutomation.py
@@ -1,34 +1,26 @@
 # HOME AUTOMATION
 import RPi.GPIO as GPIO
 
-kitchen_pin = 17
-bedroom_pin = 27
-toilet_pin = 22
+room_pins = {
+    "kitchen": 17,
+    "bedroom": 27,
+    "toilet": 22,
+}
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(27, GPIO.OUT)
-GPIO.setup(22, GPIO.OUT)
+
+#Initializing every room pin
+for room in room_pins:
+    GPIO.setup(room_pins[room], GPIO.OUT)
 
 def home_automation(reply):
-    if "kitchen" in reply:
-        print("[KITCHEN LIGHTS]")
-        if "on" in reply:
-            GPIO.output(kitchen_pin, GPIO.HIGH)
-        elif "off" in reply:
-            GPIO.output(kitchen_pin, GPIO.LOW)
-
-    elif "bedroom" in reply:
-        print("[BEDROOM LIGHTS]")
-        if "on" in reply:
-            GPIO.output(bedroom_pin, GPIO.HIGH)
-        elif "off" in reply:
-            GPIO.output(bedroom_pin, GPIO.LOW)
-
-    elif "toilet" in reply:
-        print("[TOILET LIGHTS]")
-        if "on" in reply:
-            GPIO.output(toilet_pin, GPIO.HIGH)
-        elif "off" in reply:
-            GPIO.output(toilet_pin, GPIO.LOW)
+    for room in room_pins:
+        if room in reply:
+            print(f"[{room.upper()} LIGHTS]")
+            
+            # Check if "on" or "off" is mentioned in the reply
+            if "on" in reply:
+                GPIO.output(room_pins[room], GPIO.HIGH)
+            elif "off" in reply:
+                GPIO.output(room_pins[room], GPIO.LOW)
